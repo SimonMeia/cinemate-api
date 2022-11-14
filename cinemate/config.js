8b16b6558511b67138df40c519718bd41ff7c0b5
AlexSoutoHEIG <alexandr.souto@heig-vd.ch>
2022-11-14 08:56:49 +0100

    Passwords now hashed in db

---
+++ cinemate/config.js
@@ -0,0 +1,10 @@
+// Load environment variables from the .env file.
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+// Retrieve configuration from environment variables.
+export const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1/cinemate';
+export const port = process.env.PORT || '3000';
+export const jwtSecret = process.env.JWT_SECRET || "changeme";
+export const bcryptCostFactor = 10;
+export const TMDB_API_KEY = process.env.TMDB_API_KEY
--- cinemate/routes/users.js
+++ cinemate/routes/users.js
@@ -1,104 +1,115 @@
 import express from "express";
+import bcrypt from 'bcrypt'
 import User from '../models/user.js'
 
 const router = express.Router();
 
 // Get un utilisateur from un ID
 router.get("/:id", function (req, res, next) {
-  User.findOne({ '_id': req.params.id })
-    // .populate('groups')
-    .exec(function (err, users) {
-      if (err) {
-        return next(err);
-      }
-      res.send(users);
-    });
+	User.findOne({ '_id': req.params.id })
+		// .populate('groups')
+		.exec(function (err, users) {
+			if (err) {
+				return next(err);
+			}
+			res.send(users);
+		});
 });
 
 // Get tous les utilisateurs
 router.get("/", function (req, res, next) {
-  User.find()
-    // .populate('groups')
-    .exec(function (err, users) {
-      if (err) {
-        return next(err);
-      }
-      res.send(users);
-    });
+	User.find()
+		// .populate('groups')
+		.exec(function (err, users) {
+			if (err) {
+				return next(err);
+			}
+			res.send(users);
+		});
 });
 
 // Créer un utilisateur
 router.post("/", function (req, res, next) {
-  const newUser = new User(req.body);
-  newUser.registrationDate = Date.now();
-  
-  newUser.save(function (err, savedUser) {
-    if (err) {
-      return next(err);
-    }
-    res.send(savedUser);
-  });
+	const plainPassword = req.body.password;
+	const costFactor = 10;
+
+	bcrypt.hash(plainPassword, costFactor, function (err, hashedPassword) {
+		if (err) {
+			return next(err);
+		}
+
+		const newUser = new User(req.body);
+		newUser.password = hashedPassword;
+		newUser.registrationDate = Date.now();
+		newUser.save(function (err, savedUser) {
+			if (err) {
+				return next(err);
+			}
+			res.send(savedUser);
+		});
+	});
 });
+
 
 // Supprime un utilisateur
 router.delete("/:userID", function (req, res, next) {
-  // Check si l'id est valable ? -> Fonctionne sans le check
-  User.deleteOne({ _id: req.params.userID }, function (err, user) {
-    if (err) {
-      return next(err);
-    }
-    res.send('User deleted')
-  })
+	// Check si l'id est valable ? -> Fonctionne sans le check
+	User.deleteOne({ _id: req.params.userID }, function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		res.send('User deleted')
+	})
 });
 
 // Get tous les groupes d'un utilisateur
 router.get('/:userID/groups', function (req, res, next) {
-  User.findOne({ '_id': req.params.userID })
-    .populate('groups')
-    .exec(function (err, user) {
-      if (err) {
-        return next(err);
-      }
-      res.send(user.groups);
-    });
+	User.findOne({ '_id': req.params.userID })
+		.populate('groups')
+		.exec(function (err, user) {
+			if (err) {
+				return next(err);
+			}
+			res.send(user.groups);
+		});
 })
 
 // Ajouter un utilisateur à un groupe
 router.post('/:userID/groups/:groupID', function (req, res, next) {
-  User.findOne({ _id: req.params.userID }, function (err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (!user.groups.includes(req.params.groupID)) {
-      user.groups.push(req.params.groupID)
+	User.findOne({ _id: req.params.userID }, function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		if (!user.groups.includes(req.params.groupID)) {
+			user.groups.push(req.params.groupID)
 
-      user.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-      });
-    }
-    res.send(user)
-  });
+			user.save(function (err) {
+				if (err) {
+					return next(err);
+				}
+			});
+		}
+		res.send(user)
+	});
 })
 
 // Enlever un utilisateur d'un groupe
 router.delete('/:userID/groups/:groupID', function (req, res, next) {
-  User.findOne({ _id: req.params.userID }, function (err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (user.groups.includes(req.params.groupID)) {
-      user.groups.splice(user.groups.indexOf(req.params.groupID), 1)
+	User.findOne({ _id: req.params.userID }, function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		if (user.groups.includes(req.params.groupID)) {
+			user.groups.splice(user.groups.indexOf(req.params.groupID), 1)
 
-      user.save(function (err) {
-        if (err) {
-          return next(err);
-        }
-      });
-    }
-    res.send(user)
-  });
+			user.save(function (err) {
+				if (err) {
+					return next(err);
+				}
+			});
+		}
+		res.send(user)
+	});
 })
 
 export default router;
