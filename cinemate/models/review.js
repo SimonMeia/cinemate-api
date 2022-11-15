cf95bb1748a7993861c129d80c4c6f9572e079b8
AlexSoutoHEIG <alexandr.souto@heig-vd.ch>
2022-11-15 14:57:16 +0100

    add fields for mobile hardware datas on review model

--- cinemate/models/review.js
+++ cinemate/models/review.js
@@ -22,9 +22,43 @@ const reviewSchema = new Schema({
 	movie: {
 		type: Schema.Types.ObjectId,
 		ref: 'Movie'
-	}
-	// Geoloc
-	// Media -> photo
+	},
+	location: {
+		type: {
+			type: String,
+			// required: true,
+			enum: ['Point'],
+			default: "Point"
+		},
+		coordinates: {
+			type: [Number],
+			// required: true,
+			validate: {
+				validator: validateGeoJsonCoordinates,
+				message: '{VALUE} is not a valid longitude/latitude(/altitude) coordinates array'
+			},
+			default: [6.647778558579233, 46.78060279685718]
+		}
+	},
+	medias: [{
+		type: String
+	}]
 });
+
+// Create a geospatial index on the location property.
+reviewSchema.index({ location: '2dsphere' });
+
+// Validate a GeoJSON coordinates array (longitude, latitude and optional altitude).
+function validateGeoJsonCoordinates(value) {
+	return Array.isArray(value) && value.length >= 2 && value.length <= 3 && isLongitude(value[0]) && isLatitude(value[1]);
+}
+
+function isLatitude(value) {
+	return value >= -90 && value <= 90;
+}
+
+function isLongitude(value) {
+	return value >= -180 && value <= 180;
+}
 // Create the model from the schema and export it
 export default mongoose.model('Review', reviewSchema)
