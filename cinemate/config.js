4ec2505cf1c8ef1190e2e0722f9a1bfc433d5ec9
SimonHEIG <simon.meia@heig-vd.ch>
2022-11-20 20:56:05 +0100

    add .env.example

--- cinemate/config.js
+++ cinemate/config.js
@@ -6,5 +6,5 @@ dotenv.config()
 export const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1/cinemate';
 export const port = process.env.PORT || '3000';
 export const jwtSecret = process.env.JWT_SECRET || "changeme";
-export const bcryptCostFactor = 10;
+export const bcryptCostFactor = process.env.BCRYPT_COST_FACTOR || 10;
 export const TMDB_API_KEY = process.env.TMDB_API_KEY
