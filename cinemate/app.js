a7b6c19024f0378736b75055a27b2340ff9431c5
SimonMeia <simon@meia.dev>
2023-01-27 17:03:08 +0100

    change cors policy

--- cinemate/app.js
+++ cinemate/app.js
@@ -30,6 +30,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", config.corsDomain);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(cors());
 
 app.use("/", indexRouter);
