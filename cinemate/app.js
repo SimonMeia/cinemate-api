81e477f84c4095337b52bf4ca3d7d6aa47dccc3e
SimonMeia <simon@meia.dev>
2023-01-27 17:44:13 +0100

    change cors policy

--- cinemate/app.js
+++ cinemate/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
-import fs from 'fs';
-import yaml from 'js-yaml';
-import swaggerUi from 'swagger-ui-express';
+import fs from "fs";
+import yaml from "js-yaml";
+import swaggerUi from "swagger-ui-express";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import groupsRouter from "./routes/groups.js";
@@ -13,31 +13,31 @@ import genreRouter from "./routes/genres.js";
 import moviePeopleRouter from "./routes/moviePeople.js";
 import authRouter from "./routes/auth.js";
 import * as config from "./config.js";
-import cors from 'cors'
+import cors from "cors";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 mongoose.Promise = Promise;
 mongoose.connect(config.databaseUrl);
 
 const app = express();
 
 // Parse the OpenAPI document.
-const openApiDocument = yaml.load(fs.readFileSync('./openapi.yml'));
+const openApiDocument = yaml.load(fs.readFileSync("./openapi.yml"));
 // Serve the Swagger UI documentation.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
-// app.use('/', function (req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", config.corsDomain);
-// 	res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS");
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	next();
-// });
+app.use("/", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", config.corsDomain);
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+app.use(cors());
 
 // app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -50,20 +50,18 @@ app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// Send the error status
-	res.status(err.status || 500);
-	res.send(err.message);
+    // Send the error status
+    res.status(err.status || 500);
+    res.send(err.message);
 });
 
 export default app;
